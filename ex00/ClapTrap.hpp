9ccd5473d052140f60b9cc1b7e07231194bb575f
i-r0bin <124809147+i-r0bin@users.noreply.github.com>
2024-12-15 00:24:41 +0100

    indentation

--- ex00/ClapTrap.hpp
+++ ex00/ClapTrap.hpp
@@ -11,28 +11,28 @@
 /* ************************************************************************** */
 
 #ifndef CLAPTRAP_HPP
-#define CLAPTRAP_HPP
+# define CLAPTRAP_HPP
 
-#include <string>
-#include <iostream>
+# include <string>
+# include <iostream>
 
 class ClapTrap {
-private:
-    std::string name;
-    int hitPoints;
-    int energyPoints;
-    int attackDamage;
+    private:
+        std::string name;
+        int hitPoints;
+        int energyPoints;
+        int attackDamage;
 
-public:
-    ClapTrap();
-    ClapTrap(const std::string& name);
-    ClapTrap(const ClapTrap& other);
-    ClapTrap& operator=(const ClapTrap& other);
-    ~ClapTrap();
+    public:
+        ClapTrap();
+        ClapTrap(const std::string& name);
+        ClapTrap(const ClapTrap& other);
+        ClapTrap& operator=(const ClapTrap& other);
+        ~ClapTrap();
 
-    void attack(const std::string& target);
-    void takeDamage(unsigned int amount);
-    void beRepaired(unsigned int amount);
+        void attack(const std::string& target);
+        void takeDamage(unsigned int amount);
+        void beRepaired(unsigned int amount);
 };
 
 #endif
